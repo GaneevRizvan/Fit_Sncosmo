76862940048594fe1fc51cc8ad7978f4a55dfc1c
GaneevRizvan <104025596+GaneevRizvan@users.noreply.github.com>
2024-03-04 22:32:03 +0300

    Delete Models_to_parameters.py

--- Models_to_parameters.py
+++
@@ -1,3 +0,0 @@
-list_nugent = ['nugent-sn1a', 'nugent-sn91t', 'nugent-sn91bg', 'nugent-sn1bc', 'nugent-hyper', 'nugent-sn2n', 'nugent-sn2p','nugent-sn2l']
-models_to_parameters = dict([(i, ['z', 't0', 'amplitude']) for i in list_nugent])
-models_to_parameters.update([('salt2', ['z', 't0', 'x0', 'x1', 'c'])])
